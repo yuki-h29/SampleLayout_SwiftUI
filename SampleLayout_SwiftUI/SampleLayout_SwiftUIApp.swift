87357a34254dfdc3a4e0ca76cb4fb90dceb4c324
yuki-h29 <yuki.hirano29@gmail.com>
2023-08-05 08:28:57 +0900

    Initial Commit

---
+++ SampleLayout_SwiftUI/SampleLayout_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleLayout_SwiftUIApp.swift
+//  SampleLayout_SwiftUI
+//
+//  Created by 平野裕貴 on 2023/08/05.
+//
+
+import SwiftUI
+
+@main
+struct SampleLayout_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
